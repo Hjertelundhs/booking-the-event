55df1bb5b1cca1f8a8ce0e2ea7ffd109e96927a8
Tobias Hjertelundh <tobias.hjertelundh@hotmail.com>
2019-08-16 12:35:52 +0200

    Start using graphql

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const boyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 const PORT = process.env.POST || 3000;
 
 app.use(boyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello users!');
-});
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+
+    type RootQuery {
+        events: [String!]!
+    }
+    type RootMutation {
+        createEvent(name: String): String
+    }
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`),
+    rootValue: {
+      events: () => {
+        return ['Romatic cooking', 'Sailing', 'All night coding'];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(PORT);
